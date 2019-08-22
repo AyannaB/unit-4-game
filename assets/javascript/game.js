18234456bf16b7c43c6faf8b4b046a5490747978
Ayanna Burrowes <a.burrowes529@gmail.com>
2019-08-22 17:07:45 -0400

    updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 //Initialize Variables
 var wins = 0;
 var losses = 0;
@@ -6,18 +8,19 @@ var scoreHolder = 0;
 
 var winText = document.getElementById("wins");
 var lossText = document.getElementById("losses");
-var scoreHolderText = document.getElementById("scoreHolderText");
-//Start the game
 
+//Start the game
 //Have computer select random number at start of game (between 19-120)
-var randomComputerNumber = Math.floor(Math.random() * 102) + 19;
-$("#computerNumber").html(randomComputerNumber);
+var randomComputerNumber;
+
 
 var startGame = function () {
+    randomComputerNumber = Math.floor(Math.random() * 102) + 19;
+    $("#computerNumber").text(randomComputerNumber);
     $(".crystals").empty();
+
     //Build the Crystals and attatch their images
     //Generate a random number to each crystal (between 1-12)
-
     var images = ['../images/blueCrystal.jpg',
         '../images/clearCrystal.jpg',
         '../images/pinkCrystal.jpg',
@@ -28,29 +31,41 @@ var startGame = function () {
 
         var crystal = $("<div>");
         crystal.attr({
-            "class": 'crystal',
+            // "class": 'crystal' + i,
             "random": randomCrystalNumber
         });
+        crystal.addClass(`crystal${i} crystal`);
+        // crystal.css({
+        //     "background-image": "url('" + images[i] + "')",
+        //     "background-size": "cover"
+        // });
 
-        crystal.html(randomCrystalNumber);
+        // crystal.html(randomCrystalNumber);
 
         $(".crystals").append(crystal);
     }
+    $("#scoreHolderText").html("Your Total: " + result);
 }
 startGame();
 
 //clicking each crystal should add the numbers until we reach the target number
-$(".crystal").on('click', function () {
+$(document).on('click', ".crystal", function () {
+
     var value = parseInt($(this).attr('random'));
 
     result += value;
-    scoreHolderText.textContent = scoreHolder;
-    console.log(result);
+
+    $("#scoreHolderText").html("Your Total: " + result);
+    
     //if you go over the target score, you lose/start over
+    console.log(result)
+    console.log(randomComputerNumber)
+    console.log(result > randomComputerNumber)
     if (result > randomComputerNumber) {
         alert("You Lost!!!")
         losses++;
         lossText.textContent = losses;
+        result = 0;
         startGame();
 
         //else you win and add to your win streak
@@ -59,6 +74,8 @@ $(".crystal").on('click', function () {
         alert("You Won!!!")
         wins++;
         winText.textContent = wins;
+        result = 0;
         startGame();
     }
+})
 });
